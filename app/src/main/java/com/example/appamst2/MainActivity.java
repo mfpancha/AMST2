6ab84bbb3d9827a1a5afeda4c922d4fcca8c848e
Mafer <mfpancha@espol.edu.ec>
2023-06-05 12:01:39 -0600

    commit inicial

---
+++ app/src/main/java/com/example/appamst2/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.appamst2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUsuario, edtClave;
+    private Button btnLogin, btnRegistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Referencias a los controles del diseño
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        edtClave = (EditText) findViewById(R.id.edtClave);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+    }
+
+    public void registrarse(View view) {
+        Intent intent = new Intent(this, formulario_registro.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Toast toast=Toast.makeText(getApplicationContext(),"Usted no cuenta con un usuario",Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnLogin){
+            Log.d("mensaje","ïngreso");
+
+        }else if(v.getId() == R.id.btnRegistro) {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appamst2/formulario_registro.java
@@ -0,0 +1,54 @@
+package com.example.appamst2;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class formulario_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_registro);
+    }
+
+    //insertarpaciente
+    public void insertarpaciente(View v) {
+        addNotification();
+    }
+
+    private void addNotification() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String id ="canal";
+            String description = "AMST";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(id, description, importance);
+
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(this,id)
+                            //.setSmallIcon(android.R.drawable.ic_dialog_info)
+                            .setContentTitle("Se ha registrado con exito")
+                            .setContentText("Revise su correo electronico en los proximos 2 dias")
+                            .setAutoCancel(true)
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            Intent notificationIntent = new Intent(this, formulario_registro.class);
+            notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            notificationIntent.putExtra("message", "This is a notification message");
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(pendingIntent);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            manager.createNotificationChannel(channel);
+            manager.notify(0, builder.build());
+        }}
+}
